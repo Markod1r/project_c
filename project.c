ee5e2658cb4375feda22334f0e3df3c99878ee56
marcoismakalew <marcoismakalew@gmail.com>
2024-12-03 15:31:19 +0800

    mengganti isi default case

--- project.c
+++ project.c
@@ -1,42 +1,90 @@
 #include <stdio.h>
 
-int main (){
+int selectMenu();
+float Rupiah(float a);
+float Dollar(float b);
+float Euro(float c);
+float Pound(float d);
 
-    int opsi;
-
-    do
-    {
-        
-    printf("1. option\n");
-    printf("2. option\n");
-    printf("3. option\n");
-    printf("4. option\n");
-    printf("5. exit\n");
-    printf("Pilih Option 1 - 5 = ");
-    scanf("%d", &opsi);
-
-    switch (opsi)
-    {
-    case 1:
-    printf("ini opsi satu");
-        break;
-    case 2:
-    printf("ini opsi dua");
-        break;
-    case 3:
-    printf("ini opsi tiga");
-        break;
-    case 4:
-    printf("ini opsi empat");
-        break;
-    case 5:
-    printf("Bye");
-        break;
-    default:
-    printf("opsi tidak valid");
-        break;
-    }
-
-    } while (opsi != 5);
+int main() {
+    int choice;
+    float rupiah, dollar, euro, pound;
+            
+    do {
     
+        choice = selectMenu();
+
+        switch (choice){
+        case 1:
+            printf("Masukan nominal :");
+            scanf("%f", &rupiah);
+            Rupiah(rupiah);
+            break;
+        case 2:
+            printf("Masukan nominal :");
+            scanf("%f", &dollar);
+            Dollar(dollar);
+            break;
+        case 3:
+            printf("Masukan nominal :");
+            scanf("%f", &euro);
+            Euro(euro);
+            break;
+        case 4:
+            printf("Masukan nominal :");
+            scanf("%f", &pound);
+            Pound(pound);
+            break;
+        case 5:
+            printf("==================== GOODBYE ====================\n");
+            break;
+        default:
+            printf("======= menu invalid, please try again ========\n");
+            break;
+        }
+    }while(choice != 5);
+
+    return 0;
+}
+
+int selectMenu() {
+    int choice;
+
+    printf("======== WELCOME TO CURRENCY CONVERTER ========\n");
+    printf("menu : \n");
+    printf("1. Rupiah\n");
+    printf("2. USD\n");
+    printf("3. Euro\n");
+    printf("4. Pound\n");
+    printf("5. Exit\n");
+    printf("Please select the menu : ");
+
+    scanf("%d", &choice);
+
+    return choice;
+
+}
+
+float Rupiah (float a){
+    printf("%.2f Rupiah = %.2f USD \n",a , a / 15872.11);
+    printf("%.2f Rupiah = %.2f Euro \n",a , a / 16746.58);
+    printf("%.2f Rupiah = %.2f Pound \n",a , a / 20123.76);
+}
+
+float Dollar (float b){
+    printf("%.2f USD = %.2f Rupiah \n",b , b * 15848.55);
+    printf("%.2f USD = %.2f Euro \n",b , b * 0.95);
+    printf("%.2f USD = %.2f Pound \n",b , b * 0.79);
+}
+
+float Euro (float c) {
+    printf("%.2f Euro = %.2f Rupiah \n",c , c * 16746.58);
+    printf("%.2f Euro = %.2f USD \n",c , c * 0.95);
+    printf("%.2f Euro = %.2f Pound \n",c , c * 0.83);
+}
+
+float Pound (float d){
+    printf("%.2f Pound = %.2f Rupiah \n",d , d * 20123.76);
+    printf("%.2f Pound = %.2f USD \n",d , d * 0.79);
+    printf("%.2f Pound = %.2f Euro \n",d , d * 0.83);
 }
